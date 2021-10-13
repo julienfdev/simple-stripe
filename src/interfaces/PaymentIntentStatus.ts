b4664410c4a2dbb722b447ac5e2ac2f866e0189b
Julien FERAND <hello@julienferand.dev>
2021-10-13 19:22:08 +0200

    prettier

--- src/interfaces/PaymentIntentStatus.ts
+++ src/interfaces/PaymentIntentStatus.ts
@@ -1,12 +1,12 @@
-enum PaymentIntentStatus{
-    REQUIRES_PAYMENT_METHOD,
-    REQUIRES_CONFIRMATION,
-    REQUIRES_ACTION,
-    PROCESSING,
-    REQUIRES_CAPTURE,
-    CANCELLED,
-    SUCCEEDED,
-    UNKNOWN
+enum PaymentIntentStatus {
+  REQUIRES_PAYMENT_METHOD,
+  REQUIRES_CONFIRMATION,
+  REQUIRES_ACTION,
+  PROCESSING,
+  REQUIRES_CAPTURE,
+  CANCELLED,
+  SUCCEEDED,
+  UNKNOWN,
 }
 
-export default PaymentIntentStatus
+export default PaymentIntentStatus;
